6e4ccf5507876630f7c70536ff3280606806db1c
npierre <nicolas.pierre@francetv.fr>
2021-01-09 12:22:25 +0100

    add alias

--- lib/step_flow/amqp/completed_consumer.ex
+++ lib/step_flow/amqp/completed_consumer.ex
@@ -6,7 +6,9 @@ defmodule StepFlow.Amqp.CompletedConsumer do
   require Logger
   alias StepFlow.Amqp.CompletedConsumer
   alias StepFlow.Jobs
+  alias StepFlow.Jobs.Status
   alias StepFlow.Metrics.JobInstrumenter
+  alias StepFlow.Step.Live
   alias StepFlow.Workflows
   alias StepFlow.Workflows.StepManager
 
