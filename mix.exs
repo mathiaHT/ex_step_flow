df5aeb422e8c4cb95d344cebe927359cb30620e6
Marc-Antoine Arnaud <maarnaud@media-io.com>
2021-05-19 16:03:52 +0200

    bump to 1.0.0-rc3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule StepFlow.MixProject do
   def project do
     [
       app: :step_flow,
-      version: "1.0.0-rc2",
+      version: "1.0.0-rc3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
